b9d07850b745e61e4b1f1973b50daef65a75f920
BryanJavierGarciaBarrios <145368570+BryanJavierGarciaBarrios@users.noreply.github.com>
2023-10-31 12:41:19 -0600

    Razonamiento_por_defecto

---
+++ 010_Razonamiento_por_defecto.py
@@ -0,0 +1,25 @@
+from pyknow import *
+
+class MotorDeRazonamientoPorDefecto(KnowledgeEngine):
+    @DefFacts()
+    def hechos_iniciales(self):
+        yield Fact(fiebre=True)
+        yield Fact(dolor_de_cabeza=True)
+
+    @Rule(AND(Fact(fiebre=True), Fact(dolor_de_cabeza=True)))
+    def conclusion(self):
+        print("Podría ser gripe.")
+
+    @Rule(AND(Fact(fiebre=False), Fact(dolor_de_cabeza=True)))
+    def conclusion_2(self):
+        print("Podría ser un simple dolor de cabeza.")
+
+    @Rule(AND(Fact(fiebre=True), Fact(dolor_de_cabeza=False)))
+    def conclusion_3(self):
+        print("Podría ser otra enfermedad con fiebre.")
+
+if __name__ == "__main__":
+    motor = MotorDeRazonamientoPorDefecto()
+    motor.reset()
+    motor.declare(Fact(fiebre=True))
+    motor.run()
